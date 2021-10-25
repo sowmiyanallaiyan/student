d46f7e3f96e874b6681f197ed78475de5462ef7f
sowmiyanallaiyan <sowmiya.20cse@kongu.edu>
2021-10-25 16:27:50 +0530

    insurance commit

---
+++ insdetails.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<malloc.h>
+int main()
+{
+printf("Name:sowmiya.N");
+printf("nominee name:Nallaiyan.k");
+printf("Relationship of nominee:Father");
+printf("nominee age:50");
+printf("nominee mobile number:9629655771");
+printf("nominee occupation:Manager");
+printf("nominee email:nallaiyankumarasamy@gmail.com");
+printf("nominee Adhar number:3456 1287 9702");
+}
+
