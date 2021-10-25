61856dbb31763442fe041af5eb46f340c16d2ce7
sowmiyanallaiyan <sowmiya.20cse@kongu.edu>
2021-10-25 16:32:39 +0530

    placement commit

---
+++ placedetails.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+printf("Name:sowmiya.N");
+printf("Rollno:20CSR203");
+printf("mobile number:9629655771");
+printf("personal mailid:sowmiyanallaiyan@gmail.com");
+printf("edu mailid:sowmiyan.20cse@kongu.edu");
+printf("days scholer/hosteller:hosteller");
+printf("GPA 1st sem:10");
+printf("GPA 2nd sem :10");
+printf("GPA 3rd sem:10");
+printf("CGPA upto 3rd sem:10");
+}
+
