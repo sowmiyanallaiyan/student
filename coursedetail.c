a65a32fc6c34efc81d54354e7a8246df434f9f57
sowmiyanallaiyan <sowmiya.20cse@kongu.edu>
2021-10-25 20:41:39 +0530

    course complete

---
+++ coursedetail.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+ printf("name:sowmiya.N");
+ printf("semester:3");
+ printf("no of one credit courses completed:1");
+ printf("name of the one credit course:git hub");
+ printf("number of value added course completed:1");
+ printf("name of the value added course:web development");
+ printf("NPTL result:pass"};
+}
