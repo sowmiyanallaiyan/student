bc34e10f36846d4b5b370dbaa23c82d467209ab2
sowmiyanallaiyan <sowmiya.20cse@kongu.edu>
2021-10-25 16:23:13 +0530

    first commit

---
+++ details.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<malloc.h>
+int main()
+{
+
+printf("Name:sowmiya.N");
+printf("rollno:20CSR203");
+printf("Fathername:Nallaiyan.k");
+printf("Mothername:Amudha.c");
+printf("age:19");
+printf("MObile number:9629655771");
+printf("email:sowmiyan.20cse@kongu.edu");
+printf("blood group:O+ve");
+printf("Address:1/220A,periya ayyampalayam,namakkal");
+printf("adhar number:2385 2906 1257");
+}
+
